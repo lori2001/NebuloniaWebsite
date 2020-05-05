91939929c6ed7c6b819c2b925127f24336075e83
Szoke Lorand <lori.szoke@yahoo.com>
2020-05-05 14:06:07 +0300

    onedrive added to archive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
 
     // IMPORTANT: Current layout supports only two languages
     // Ask me(Szoke Lorand(creators' page)) if you want to add a third language (because it's tricky to get right)
-    translate.addLangs(['huV2', 'roV2']);
+    translate.addLangs(['huV3', 'roV3']);
 
     translate.setDefaultLang(this.translate.getLangs()[0]);
     this.translate.currentLang = this.translate.getLangs()[0];
