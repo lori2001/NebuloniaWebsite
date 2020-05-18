98fc97a73765d1fd011a751f583e4d1a230ae3e5
Szoke Lorand <lori.szoke@yahoo.com>
2020-05-19 02:01:56 +0300

    version update for languages (avoid cacheing)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
 
     // IMPORTANT: Current layout supports only two languages
     // Ask me(Szoke Lorand(creators' page)) if you want to add a third language (because it's tricky to get right)
-    translate.addLangs(['huV6', 'roV6']);
+    translate.addLangs(['huV7', 'roV7']);
 
     translate.setDefaultLang(this.translate.getLangs()[0]);
     this.translate.currentLang = this.translate.getLangs()[0];
